f0f7322945eee330a6a59f94918f52b7f6429bb1
nssuperx <mgtmgtx@gmail.com>
2020-10-21 17:04:00 +0900

    少し整形した．

--- nmf_2020/nmf_sklearn.py
+++ nmf_2020/nmf_sklearn.py
@@ -19,8 +19,8 @@ def main():
     W = nmf.fit_transform(V)
     # これを，可視化する．棒グラフを作る．
     H = nmf.components_
-    print("W shape:" + str(W.shape))
     print("V shape:" + str(V.shape))
+    print("W shape:" + str(W.shape))
     print("H shape:" + str(H.shape))
     print("iter:%d" % (nmf.n_iter_))
     
@@ -28,27 +28,38 @@ def main():
     W_image = W.T.reshape(r, 28, 28)
 
     # ここからグラフ作成
-    sample_num = 10
+    sample_num = 5
     sample_list = np.random.randint(0, m, size=sample_num)
     figrow_master = sample_num + 1
     figcol_master = r + 1
     fig = plt.figure(figsize=(figrow_master, figcol_master))
+    plt.rcParams['axes.xmargin'] = 0
     gs_master = GridSpec(nrows=figrow_master, ncols=figcol_master)
+
+    # 選択したオリジナル画像
     gs_1 = GridSpecFromSubplotSpec(nrows=sample_num, ncols=1, subplot_spec=gs_master[0:sample_num, 0])
     for i in range(0, sample_num):
         img = V[:, sample_list[i]].reshape(28, 28)
         ax = fig.add_subplot(gs_1[i, :])
+        ax.axes.xaxis.set_visible(False)
+        ax.axes.yaxis.set_visible(False)
         ax.imshow(img)
 
+    # 棒グラフ
     gs_2 = GridSpecFromSubplotSpec(nrows=sample_num, ncols=r, subplot_spec=gs_master[0:sample_num, 1:figcol_master])
     for i in range(0, sample_num):
         ax = fig.add_subplot(gs_2[i, :])
+        ax.axes.xaxis.set_visible(False)
         ax.bar(range(r), H[:, sample_list[i]])
 
+    # 基底画像
     gs_3 = GridSpecFromSubplotSpec(nrows=1, ncols=r, subplot_spec=gs_master[sample_num, 1:figcol_master])
     for i in range(0, r):
         ax = fig.add_subplot(gs_3[:, i])
+        ax.axes.xaxis.set_visible(False)
+        ax.axes.yaxis.set_visible(False)
         ax.imshow(W_image[i])
+
     plt.show()
     
     # 基底を作るときに使ってない画像は復元できない？NMFの弱点？
