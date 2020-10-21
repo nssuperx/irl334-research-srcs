1005b770a79a77977a863d07b1550c17c7fc4ae4
nssuperx <mgtmgtx@gmail.com>
2020-10-21 16:28:31 +0900

    だいたい完成

--- nmf_2020/nmf_sklearn.py
+++ nmf_2020/nmf_sklearn.py
@@ -1,7 +1,6 @@
-import random
-import math
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec, GridSpecFromSubplotSpec
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import NMF
@@ -16,33 +15,43 @@ r = 10
 def main():
     V, V_test, label, label_test = setup_mnist()
     nmf = NMF(n_components=r, max_iter=400)
-    # nmf.fit(V)
-    print("V shape:" + str(V.shape))
-
-    W = nmf.fit_transform(V)
-    print("W shape:" + str(W.shape))
-
     # 基底ベクトルを取得
+    W = nmf.fit_transform(V)
+    # これを，可視化する．棒グラフを作る．
     H = nmf.components_
+    print("W shape:" + str(W.shape))
+    print("V shape:" + str(V.shape))
     print("H shape:" + str(H.shape))
-
     print("iter:%d" % (nmf.n_iter_))
     
-    H_image = H.reshape(r, 28, 28)
+    # 基底ベクトルを画像のように整形
+    W_image = W.T.reshape(r, 28, 28)
 
-    # 画像の表示
-    fig = plt.figure()
+    # ここからグラフ作成
+    sample_num = 10
+    sample_list = np.random.randint(0, m, size=sample_num)
+    figrow_master = sample_num + 1
+    figcol_master = r + 1
+    fig = plt.figure(figsize=(figrow_master, figcol_master))
+    gs_master = GridSpec(nrows=figrow_master, ncols=figcol_master)
+    gs_1 = GridSpecFromSubplotSpec(nrows=sample_num, ncols=1, subplot_spec=gs_master[0:sample_num, 0])
+    for i in range(0, sample_num):
+        img = V[:, sample_list[i]].reshape(28, 28)
+        ax = fig.add_subplot(gs_1[i, :])
+        ax.imshow(img)
+
+    gs_2 = GridSpecFromSubplotSpec(nrows=sample_num, ncols=r, subplot_spec=gs_master[0:sample_num, 1:figcol_master])
+    for i in range(0, sample_num):
+        ax = fig.add_subplot(gs_2[i, :])
+        ax.bar(range(r), H[:, sample_list[i]])
+
+    gs_3 = GridSpecFromSubplotSpec(nrows=1, ncols=r, subplot_spec=gs_master[sample_num, 1:figcol_master])
     for i in range(0, r):
-        ax = fig.add_subplot(2, 5, i+1)
-        ax.imshow(H_image[i])
+        ax = fig.add_subplot(gs_3[:, i])
+        ax.imshow(W_image[i])
     plt.show()
     
-    # このW_testのヒストグラムを書いてみる．
-    W_test = nmf.transform(V_test)
-    print(W_test[0])
-    print(W_test[1])
-    print(W_test[2])
-    print(W_test[3])
+    # 基底を作るときに使ってない画像は復元できない？NMFの弱点？
 
 
 def setup_mnist():
@@ -52,7 +61,7 @@ def setup_mnist():
     digits = fetch_openml(name='mnist_784', version=1, data_home="mnist")
     x_train, x_test, y_train, y_test = train_test_split(digits.data, digits.target, train_size=m, random_state=0, shuffle=True)
 
-    return x_train, x_test, y_train, y_test
+    return x_train.T, x_test.T, y_train, y_test
 
 if __name__ == "__main__":
     main()
