384617a03feff3237c81f4fe28a9ef7d1587e9cd
nssuperx <mgtmgtx@gmail.com>
2021-01-22 19:00:40 +0900

    update: pca

--- nmf_2020/pca_sklearn.py
+++ nmf_2020/pca_sklearn.py
@@ -5,6 +5,8 @@ from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 
+from modules.data_function import setup_mnist
+
 # http://yann.lecun.com/exdb/mnist/
 # The training set contains 60000 examples, and the test set 10000 examples.
 
@@ -12,10 +14,10 @@ from sklearn.decomposition import PCA
 
 n = 28 * 28     # 画素数
 m = 10000       # 画像数
-r = 10
+r = 100
 
 def main():
-    V, V_test, label, label_test = setup_mnist()
+    V, label = setup_mnist(image_num=m)
     pca = PCA(n_components=r)
     # 基底ベクトルを取得
     W = pca.fit_transform(V)
@@ -25,9 +27,29 @@ def main():
     print("W shape:" + str(W.shape))
     print("H shape:" + str(H.shape))
     
+    reconstruct_V = pca.inverse_transform(W)
+    print("reconstruct_V.shape: " + str(reconstruct_V.shape))
+    
     # 基底ベクトルを画像のように整形
     W_image = W.T.reshape(r, 28, 28)
 
+    sample_num = 5
+    sample_index_list = np.random.randint(0, m, size=sample_num)
+    fig = plt.figure()
+    for i in range(0, sample_num):
+        img = V[:,sample_index_list[i]].reshape(28, 28)
+        ax = fig.add_subplot(2,5,i+1)
+        ax.axes.xaxis.set_visible(False)
+        ax.axes.yaxis.set_visible(False)
+        ax.imshow(img)
+        img = reconstruct_V[:,sample_index_list[i]].reshape(28, 28)
+        ax = fig.add_subplot(2,5,i+6)
+        ax.axes.xaxis.set_visible(False)
+        ax.axes.yaxis.set_visible(False)
+        ax.imshow(img)
+    plt.show()
+
+    '''
     # ここからグラフ作成
     sample_num = 5
     sample_index_list = np.random.randint(0, m, size=sample_num)
@@ -72,17 +94,8 @@ def main():
         ax.imshow(W_image[i])
 
     plt.show()
+    '''
     
-
-def setup_mnist():
-    """
-    sklearnでmnistのデータを作り，numpy配列を作る．
-    """
-    digits = fetch_openml(name='mnist_784', version=1, data_home="mnist")
-    x_train, x_test, y_train, y_test = train_test_split(digits.data, digits.target, train_size=m, random_state=0, shuffle=True)
-
-    return x_train.T, x_test.T, y_train, y_test
-
 if __name__ == "__main__":
     main()
     
