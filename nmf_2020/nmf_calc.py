69cc3eccaded7391ee542375d6938119a3a84a47
nssuperx <mgtmgtx@gmail.com>
2021-02-07 19:13:24 +0900

    update: m=60000で実験

--- nmf_2020/nmf_calc.py
+++ nmf_2020/nmf_calc.py
@@ -8,9 +8,10 @@ from modules.data import setup_mnist, csv_out, setup_face
 from modules.visualize import show_base_grid, show_reconstruct_pairs, show_base_weight, show_graph
 from modules.array import normalize, search_near_imagepair
 
-m = 1000       # 画像数
+m = 60000       # 画像数
 # r_list = [10, 49, 100, 439, 784, 1000]
-r_list = [439] # 基底数 決め方:(n + m)r < nm
+r_list = [10, 49, 100, 773, 1000]
+# r_list = [439] # 基底数 決め方:(n + m)r < nm
 iteration = 200
 
 def main():
@@ -22,18 +23,18 @@ def main():
     for r in r_list:
         print("r = " + str(r))
         nmf = NMF(seed=0)
-        nmf.calc(V, r, iteration, save=False, use_cache=True)
+        nmf.calc(V, r, iteration, save=False, use_cache=False)
         W = nmf.W
         H = nmf.H
         F_list = nmf.loss_LOG
-        # csv_out('nmf_r' + str(r) + '.csv', ('iteration', 'F'), (range(1, iteration + 1), F_list))
+        csv_out('nmf_r' + str(r) + '_m60000.csv', ('iteration', 'F'), (range(1, iteration + 1), F_list))
 
         # show_graph(range(iteration), F_list, 'iteration', 'F')
 
         reconstruct_V = np.dot(W, H)
         # show_base_grid(W, r, horizontal_num=5, vertical_num=2, img_cmap="Greens", img_normalize=True, save_img=False, filename=None, show_img=True)
-        # show_base_grid(W, r, img_cmap="Greens", img_normalize=True)
-        # show_reconstruct_pairs(V, reconstruct_V, m, img_cmap='Greys', separate=True)
+        show_base_grid(W, r, img_cmap="Greens", img_normalize=True, save_img=True, filename='nmf_r' + str(r) + '_grid_m60000.pdf', show_img=False)
+        show_reconstruct_pairs(V, reconstruct_V, m, img_cmap='Greys', separate=True, save_img=True, filename='nmf_r' + str(r) + '_reconstruct_m60000.pdf', show_img=False)
         # show_base_weight(V, reconstruct_V, W, H, r, m)
 
         # TODO: 以下，関数化
