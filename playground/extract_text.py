1f36e23740ca9f3da0af10f862dad55a822418b2
nssuperx <40332687+nssuperx@users.noreply.github.com>
2021-09-10 01:00:25 +0900

    update: pdfからテキスト抽出

--- playground/extract_text.py
+++ playground/extract_text.py
@@ -1,14 +1,14 @@
 from pdfminer.high_level import extract_text
 import sys
 import pathlib
-import os
 
 args = sys.argv
 input_filepath: str = args[1]
 
 text = extract_text(input_filepath)
 
-# out_filepath = 
+p = pathlib.Path(input_filepath)
+out_filepath = p.with_suffix(".txt")
 
-with open(path_w, mode='w') as f:
-    f.write(s)
+with open(out_filepath, mode='w', encoding="utf-8") as f:
+    f.write(text)
--- playground/pathlib_test.py
+++ playground/pathlib_test.py
@@ -1,15 +1,15 @@
 import pathlib
-import sys
 
-args = sys.argv
-input_filepath: str = args[1]
+input_filepath: str = r'd:\Users\test\test.pdf'
+
+print(input_filepath)
 
 p = pathlib.PureWindowsPath(input_filepath)
 print(p.name)
 print(p.stem)
 print(p.suffix)
 print(p.parent)
-print(p.with_suffix(".txt"))
+print(p.with_suffix(".txt").as_posix())
 
 outfilename = pathlib.Path(p.name)
 outfilepath = outfilename.with_suffix(".txt")
