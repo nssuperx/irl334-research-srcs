14816cfbcb2c1857e419ca6c974a8a02f83adcdf
nssuperx <40332687+nssuperx@users.noreply.github.com>
2021-10-25 15:38:14 +0900

    update: 正規化など

--- invariance/playground.py
+++ invariance/playground.py
@@ -2,9 +2,27 @@ from typing import ContextManager, Dict
 from PIL import Image
 import numpy as np
 
+"""
+テストの仕方
+全部反転させれば，一番相関が高かったところが低くなる
+"""
+
+class TemplateImage:
+    img: np.ndarray
+    mean: float
+    variance: float
+    sd: float
+
+    def __init__(self, imgArray: np.ndarray) -> None:
+        self.img = imgArray
+        self.mean = self.img.mean()
+        self.variance = self.img.var()
+        self.sd = self.img.std()
+    
+
 def main():
     imgDic = read_image()
-    test(imgDic)
+    # image_read_test(imgDic)
     originalImgArray = imgDic["original"]
     rightEyeImgArray = imgDic["right_eye"]
     leftEyeImgArray = imgDic["left_eye"]
@@ -21,7 +39,9 @@ def main():
         for j in range(rightScanImgArray.shape[1]):
             # 相関係数出す範囲をスライス
             scanTargetImgArray = originalImgArray[i:i+rightEyeImgArray.shape[0], j:j+rightEyeImgArray.shape[1]]
-            rightScanImgArray[i][j] = np.corrcoef(scanTargetImgArray.flatten(), rightEyeImgArray.flatten())[0][1]
+            cov = np.mean(np.multiply(scanTargetImgArray, rightEyeImgArray))
+            # rightScanImgArray[i][j] = np.corrcoef(scanTargetImgArray.flatten(), rightEyeImgArray.flatten())[0][1]
+            rightScanImgArray[i][j] = cov / (scanTargetImgArray.std() * rightEyeImgArray.std())
     
     for i in range(leftScanImgArray.shape[0]):
         for j in range(leftScanImgArray.shape[1]):
@@ -34,17 +54,17 @@ def main():
     np.save("./rightScanImgTmp", rightScanImgArray)
     rightimg = Image.fromarray(rightScanImgArray * 255).convert("L")
     rightimg.show()
-    rightimg.save("./rightScanImg.png")
+    rightimg.save("./images/out/rightScanImg.png")
 
     # leftScanImgArray = np.load("./leftScanImgTmp.npy")
     np.save("./leftScanImgTmp", leftScanImgArray)
     leftimg = Image.fromarray(leftScanImgArray * 255).convert("L")
     leftimg.show()
-    leftimg.save("./leftScanImg.png")
+    leftimg.save("./images/out/leftScanImg.png")
 
 
 
-def read_image() -> dict:
+def read_image(normalize: bool = True) -> dict:
     """
     画像を読み込む
 
@@ -54,22 +74,30 @@ def read_image() -> dict:
         key: 画像の種類(original, right_eye, left_eye)
         value: numpy.adarray: 読み込んだ画像
     """
+    originalImagePath = "./images/in/sample.png"
+    rightEyeImagePath = "./images/in/right_eye.png"
+    leftEyeImagePath = "./images/in/left_eye.png"
 
-    originalImagePath = "./images/sample.png"
-    rightEyeImagePath = "./images/right_eye.png"
-    leftEyeImagePath = "./images/left_eye.png"
+    if normalize:
+        originalImage = np.array(Image.open(originalImagePath))
+        rightEyeImage = np.array(Image.open(rightEyeImagePath))
+        leftEyeImage = np.array(Image.open(leftEyeImagePath))
 
-    # 画像読み込みつつndarrayに変換，asarray()を使うとread-onlyなデータができる．
-    originalImage = np.asarray(Image.open(originalImagePath))
-    rightEyeImage = np.asarray(Image.open(rightEyeImagePath))
-    leftEyeImage = np.asarray(Image.open(leftEyeImagePath))
+        originalImage = (originalImage - originalImage.mean()) / originalImage.std()
+        rightEyeImage = (rightEyeImage - rightEyeImage.mean()) / rightEyeImage.std()
+        leftEyeImage = (leftEyeImage - leftEyeImage.mean()) / leftEyeImage.std()
+    else:
+        # 画像読み込みつつndarrayに変換，asarray()を使うとread-onlyなデータができる．
+        originalImage = np.asarray(Image.open(originalImagePath))
+        rightEyeImage = np.asarray(Image.open(rightEyeImagePath))
+        leftEyeImage = np.asarray(Image.open(leftEyeImagePath))
 
     imgDic = {"original": originalImage, "right_eye": rightEyeImage, "left_eye": leftEyeImage}
 
     return imgDic
 
 
-def test(imgDic: dict):
+def image_read_test(imgDic: dict):
     """
     画像を読み込めたかテストする
 
--- invariance/rgb_to_glay.py
+++ invariance/rgb_to_glay.py
@@ -1,10 +1,9 @@
-from os import sched_get_priority_max
 from PIL import Image
 
 def main():
-    inoutPairfilePaths = (("./images/original/sample.png", "./images/sample.png"),
-        ("./images/original/right_eye.png", "./images/right_eye.png"),
-        ("./images/original/left_eye.png", "./images/left_eye.png"))
+    inoutPairfilePaths = (("./images/original/sample_small.png", "./images/in/sample.png"),
+        ("./images/original/right_eye_small.png", "./images/in/right_eye.png"),
+        ("./images/original/left_eye_small.png", "./images/in/left_eye.png"))
     
     for pair in inoutPairfilePaths:
         img = Image.open(pair[0])
