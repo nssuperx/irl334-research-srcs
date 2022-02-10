7c77b571b9915dcf3255b9ba6189dd61654a0667
nssuperx <40332687+nssuperx@users.noreply.github.com>
2022-02-10 21:24:55 +0900

    update: 一回のタスクを大きくしたけどそんなに速くならなかった

--- hmm/different_xmap_mp.py
+++ hmm/different_xmap_mp.py
@@ -1,9 +1,25 @@
+from enum import Enum, auto
 from typing import List
 import numpy as np
 import matplotlib.pyplot as plt
 from multiprocessing import Pool
+from itertools import product
+from logging import getLogger, StreamHandler, INFO, Formatter, CRITICAL
+import time
 
 from hmm_class import HMM
+
+# logger setting
+logger = getLogger()
+handler = StreamHandler()
+handler.setFormatter(Formatter('%(asctime)s [%(levelname)s]: %(message)s'))
+handler.setLevel(INFO)
+logger.addHandler(handler)
+logger.setLevel(INFO)
+
+class MP_Method(Enum):
+    ONE = auto()
+    BATCH = auto()
 
 class trial_result:
     def __init__(self, p00: float, p11: float, hamming_distance: float) -> None:
@@ -11,26 +27,40 @@ class trial_result:
         self.p11 = p11
         self.hamming_distanse = hamming_distance
 
-def hmm_task(n: int, sigma: float, p00: float, p11: float) -> int:
+MP_TYPE = MP_Method.BATCH
+
+def hmm_task(n: int, sigma: float, p00: float, p11: float, trial_num: int) -> trial_result:
+    start_time = time.time()
+    hamming_distanse_list = []
     hmm = HMM(n, sigma, 0.97, 0.99, p00, p11)
-    hmm.generate_x()
-    hmm.generate_y()
-    hmm.compute_xmap()
-    return calc_hamming(hmm)
+    for i in range(trial_num):
+        hmm.generate_x()
+        hmm.generate_y()
+        hmm.compute_xmap()
+        hamming_distanse_list.append(calc_hamming(hmm))
+    hamming_mean = np.asarray(hamming_distanse_list, dtype=np.float64).mean()
+    logger.info(f'process time: {time.time() - start_time}')
+    return trial_result(p00, p11, hamming_mean)
 
 def main():
     n = 200
     sigma = 0.7
     results: List[trial_result] = []
 
-    for p00 in np.arange(0.50, 1.00, 0.01):
-        for p11 in np.arange(0.50, 1.00, 0.01):
-            hamming_distanse = []
-            hmm_args = [(n, sigma, p00, p11) for i in range(1000)]
-            with Pool(processes=6) as pool:
-                hamming_distanse = pool.starmap(hmm_task, hmm_args)
-            hamming_mean = np.asarray(hamming_distanse, dtype=np.float64).mean()
-            results.append(trial_result(p00, p11, hamming_mean))
+    if MP_TYPE == MP_Method.BATCH:
+        p_args = list(product(np.arange(0.50, 1.00, 0.01), np.arange(0.50, 1.00, 0.01)))
+        hmm_args = [(n, sigma, arg[0], arg[1], 1000) for arg in p_args]
+        with Pool(processes=6) as pool:
+            results = pool.starmap(hmm_task, hmm_args)
+        logger.info(f'p_args len: {len(results)}')
+    else:
+        for p00 in np.arange(0.50, 1.00, 0.01):
+            for p11 in np.arange(0.50, 1.00, 0.01):
+                hmm_args = [(n, sigma, p00, p11) for i in range(1000)]
+                with Pool(processes=6) as pool:
+                    hamming_distanse = pool.starmap(hmm_task, hmm_args)
+                hamming_mean = np.asarray(hamming_distanse, dtype=np.float64).mean()
+                results.append(trial_result(p00, p11, hamming_mean))
 
     array_x = np.zeros((len(results)))
     array_y = np.zeros((len(results)))
