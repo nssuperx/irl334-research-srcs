fd61898c8d2feae2a549bd2b244601d49a9bc574
nssuperx <40332687+nssuperx@users.noreply.github.com>
2022-03-30 18:30:11 +0900

    update: 7問目 完成

--- hmm/hmm07.py
+++ hmm/hmm07.py
@@ -1,3 +1,4 @@
+from typing import List
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
@@ -5,51 +6,47 @@ from tqdm import tqdm
 from hmm_class import HMM, Model
 
 n = 200
-hmm_num = 1000
-theta_first = 0.7
-theta_last = 0.99
-theta_step = 0.05
+sigma = 0.7
+trial_num = 1000
+theta_first = 0.900
+theta_last = 0.999
+theta_step = 0.001
 
 def hmm07() -> None:
-    sigma = 0.7
     theta_list = np.arange(theta_first, theta_last, theta_step)
-    mean_list = []
-    std_list_up = []
-    std_list_down = []
+    error_list = []
 
-    # おおもとのxとyを生成．固定しておく．
-    hmm = HMM(n, sigma, 0.99, 0.97)
-    hmm.generate_x()
-    hmm.generate_y()
+    for i in tqdm(range(trial_num)):
+        error = task(theta_list)
+        error_list.append(error)
+    
+    error_array = np.array(error_list)
+    mean_array = error_array.mean(axis=0, dtype=np.float64)
+    std_array = error_array.std(axis=0, dtype=np.float64)
 
-    for theta in tqdm(theta_list):
-        mean, std = task(hmm, theta)
-        mean_list.append(mean)
-        std_list_up.append(mean + std)
-        std_list_down.append(mean - std)
-
-    plt.plot(theta_list, mean_list, label='mean')
-    plt.plot(theta_list, std_list_up, label='std+')
-    plt.plot(theta_list, std_list_down, label='std-')
+    plt.plot(theta_list, mean_array, label='mean')
+    plt.plot(theta_list, mean_array + std_array, label='std+')
+    plt.plot(theta_list, mean_array - std_array, label='std-')
 
     plt.title('hamming distanse') 
     plt.xlabel('theta')
-    plt.ylabel('y')
-    plt.xticks(np.arange(min(theta_list), max(theta_list) + 0.01, theta_step))
+    plt.ylabel('error')
+    # plt.xticks(np.arange(min(theta_list), max(theta_list) + 0.01, theta_step))
     plt.legend()
 
     plt.show() # 描画
 
-def task(hmm: HMM, theta: float) -> tuple[float, float]:
-    hamming_distanse = np.empty(hmm_num)
-    
-    for i in tqdm(range(hmm_num), leave=False):
-        # 変更するのはxmapを計算するときに使うモデルのみ
+def task(theta_list: np.ndarray) -> List[int]:
+    error: List[float] = []
+    hmm = HMM(n, sigma, 0.99, 0.97)
+    hmm.generate_x()
+    hmm.generate_y()
+    for theta in tqdm(theta_list, leave=False):
         hmm.model_xmap = Model(theta, theta)
         hmm.compute_xmap()
-        hamming_distanse[i] = hmm.calc_error()
+        error.append(hmm.calc_error())
 
-    return np.mean(hamming_distanse), np.std(hamming_distanse)
+    return error
 
 
 if __name__ == '__main__':
