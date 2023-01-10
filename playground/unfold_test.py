f88adbaa6263a993ced779371238aabd0006c5ba
nssuperx <40332687+nssuperx@users.noreply.github.com>
2023-01-10 12:40:25 +0900

    update: 論文用の図生成

--- playground/unfold_test.py
+++ playground/unfold_test.py
@@ -20,25 +20,28 @@ test_dataset = datasets.MNIST(
 MNIST_classes = datasets.MNIST.classes
 
 batch_size = 10
+kernel_size = 8
+stride = 4
+kernel_row = (28 - kernel_size + stride) // stride
 
 dl = DataLoader(train_dataset, batch_size, shuffle=False)
 
 it = iter(dl)
 image, _ = next(it)
 
-torchvision.utils.save_image(image, "orig.png", normalize=True)
+torchvision.utils.save_image(image, f"orig{batch_size}.png", normalize=True)
 
-miniimg: torch.Tensor = image.unfold(2, 4, 2).unfold(3, 4, 2)  # torch.Size([1, 1, 13, 13, 4, 4])
+miniimg: torch.Tensor = image.unfold(2, kernel_size, stride).unfold(3, kernel_size, stride)
 
 # 直感でわかりやすいUnfoldした画像の作り方．オリジナル画像1枚を単位としてまとめたもの
 miniimg = miniimg.permute((0, 2, 3, 1, 4, 5))
-miniimg = miniimg.reshape(batch_size, 13 * 13, 4, 4)
-torchvision.utils.save_image(miniimg[1].unsqueeze(dim=1), "test.png", nrow=13, normalize=True)
+miniimg = miniimg.reshape(batch_size, kernel_row**2, kernel_size, kernel_size)
+torchvision.utils.save_image(miniimg[0].unsqueeze(dim=1), "test.png", nrow=kernel_row, normalize=True)
 test = miniimg.detach().clone()[1]
 
 # バッチ枚数分画像を重ねて，重ねたままカットして，それを区画ごとにまとめるイメージ
 miniimg = miniimg.permute((1, 0, 2, 3))
-torchvision.utils.save_image(miniimg[:, 1].unsqueeze(dim=1), "test2.png", nrow=13, normalize=True)
+torchvision.utils.save_image(miniimg[:, 1].unsqueeze(dim=1), "test2.png", nrow=kernel_row, normalize=True)
 test2 = miniimg.detach().clone()[:, 1]
 
 diff = test - test2
